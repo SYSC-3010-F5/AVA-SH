8d854f634128cde86f7485d9b1ecf603833c3c29
Light-Of-Darkness <Light-Of-Darkness@users.noreply.github.com>
2017-04-01 17:24:31 -0400

    Switching over to the force sensor
    Initial code for testing the sensor

--- src/coffee-maker/scale.py
+++ src/coffee-maker/scale.py
@@ -1,51 +1,33 @@
-VENDOR_ID = 0x0922
-devices = usb.core.find(find_all=True, idVendor=VENDOR_ID)
+#!/usr/bin/python
+from time import sleep
 
-      for device in devices:   
-         if device.is_kernel_driver_active(0) is True:
-            device.detach_kernel_driver(0)
-         devbus = str(device.bus)
-         devaddr = str(device.address)
-         productid=str(device.idProduct)
-         try:
-            if str(usb.util.get_string(device,256,3)) == serialno:
-               if debug: print "scale id:" + id + " serial: "+ serialno
-               if debug: print ("device serial:    <" + str(usb.util.get_string(device,256,3))) + ">"
-               ## set USB device endpoint here
-               endpoint = device[0][(0,0)][0]
-               # read a data packet
-               attempts = 10
-               data = None                  
-               while data is None:# and attempts > 0:
-                  try:
-                     data = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
-                     if debug: print "data: "+str(data)
-                  except usb.core.USBError as e:
-                     data = None
-                     if e.args == ('Operation timed out',):
-                        attempts -= 1
-                        print e
-                        continue
-               
-               # The raw scale array data
-               #print data
-               raw_weight = data[4] + (256 * data[5])
+import subprocess
 
-               if data[2] == DATA_MODE_OUNCES:
-                  ounces = raw_weight * 0.1
-                  weight = "%s oz" % ounces
-               elif data[2] == DATA_MODE_GRAMS:
-                  grams = raw_weight
-                  weight = "%s g" % grams
-                  
-               reading = weight
-               if debug: print "raw reading '" + reading +"'"
-               readval = float(reading.split(" ")[0])
-               readunit = reading.split(" ")[1]
-               ## if the units are ounces ("oz") then convert to "g"
-               if readunit == "oz" and readval !=0:
-                  readval = readval*28.3495
-                  if debug: print "converted oz to g"
-               if debug: print "current weight : '" + str(readval) +"' "+readunit
-               if debug: print "current time   : "+strftime("%Y-%m-%d %H:%M:%S", localtime())
-               
+#to prevent SPI failures, reload spi drivers at the beginning
+#only needed for Rpi2
+#unload_spi = subprocess.Popen('sudo rmmod spi_bcm2708', shell=True, stdout=subprocess.PIPE)
+#start_spi = subprocess.Popen('sudo modprobe spi_bcm2708', shell=True, stdout=subprocess.PIPE)
+sleep(3)
+
+#import spidev and open SPI to work with ADC on Gertboard
+import spidev
+
+loops = 600
+
+spi = spidev.SpiDev()
+#the ADC of the gertboard is on SPI channel 0
+spi.open(0,0)
+#select the channel of the AD: 0 for AD0, 1 for AD1
+channel = 1
+
+while True:
+    #send start bit, sgl/diff, odd/sign, MSBF to SPI
+    r = spi.xfer2([1, (2+channel)<<6, 0])
+    #spi.xfer2 returns same number of 8bit bytes as sent
+    #parse out the part of bits which includes the changing value of the sensor
+    adc_value = ((r[1]&31) << 6) + (r[2] >> 2)
+    #print analogue value
+    print(adc_value)
+    #delay after each print
+    sleep(0.05)
+    loops -= 1
