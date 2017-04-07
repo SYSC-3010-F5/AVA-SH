67386182704d5969fac67579bd5ea2de60fcdf21
sarahgarlough <sarahgarlough@users.noreply.github.com>
2017-04-06 20:21:06 -0400

    Updated thermo script
    Added multi-module on one pi, and comments

--- src/thermo.py
+++ src/thermo.py
@@ -1,75 +1,68 @@
 import pygame
 import time
-import pifacedigitalio as pfio
 import dataChannel
 
 #Initalizations
 connected=False
-pfio.init()
-
-
-#used for server alarm off
-alarmOff=False
-
-
-#button poolling
-def checkButton():
-	if(pfio.digital_read(1) != 0 or pfio.digital_read(2) != 0 or pfio.digital_read(3) != 0):
-                print('Button Pushed')
-                return True
-        else:
-                return False
-def getTemp():
-    myChannel.sendInfo(temp)
-    
-def setTemp(newTemp):
-    temp=newTemp
-
-def systemOff():
-    running=False
-
-def heatOn():
-    heater=True
-
-def acOn():
-    ac=True
-    
-def statusCheck()
-    
-
-def sortPack(info, extra):
-        if(info=='check temp'):
-                getTemp()
-        elif(info=='change temp'):
-                setTemp(extra)
-        elif(info=='off'):
-                systemOff()
-                
-    
-
-myChannel=dataChannel.DataChannel()
-name='Alarm Controller'
+serverIP='192.168.2.100' #Set to IP of server
+serverPort=3010 #set to Port of server
+name='t\Thermostat' #thermostat modules alway begin with t\
+myChannel=dataChannel.DataChannel(name, 55000)
 compNumber=0
 trys=0
-ledOff()
+
+
+# get current temp of system
+def getTemp():
+        print ('Current get temp:'+ str(temp))
+        return temp 
+    
+
+def setTemp(newTemp):
+        global temp
+        temp=newTemp
+        print ('Temperature changing to:'+ str(temp))
+
+#turn system off (no longer monitoring
+def systemOff():
+        running=False
+        print('System off')
+
+def heatOn():#easily added in
+        heater=True
+        print('heating')
+        temp+=1
+
+def acOn():#easily added in
+        ac=True
+        print('System Cooling')
+        temp-=1
+
+    
+#sort messages
+def sortPack(info, extra):
+        if(info=='new temp'):
+                setTemp(extra)
+        elif(info=='system off'):
+                systemOff() 
+    
+connected = myChannel.connect(serverIP, serverPort, (name))#create channel
 while(True):
-        connected = myChannel.connect("134.117.58.116", 3010, (name))
 
         while(connected==True):
-                data, addr = myChannel.rcvPacket(None)
+                data, addr = myChannel.rcvPacket(None)#wait forever for packet
+                print('Connected to Terminal')
                 opcode, info, extra = myChannel.Unpack(data)
 
                 sortPack(info,extra)
-
-                connected=True
-                statusCheck()
         
-        trys=trys+1
+        trys=trys+1#not connected, attempt to try again
         if(trys==100):
+        else:
                 print ('10 attemps, changing name')
                 compNumber+=1
                 newName=name+str(compNumber)
-                print newName
+                connected = myChannel.connect(serverIP, serverPort, (newName))
                 trys=0
 
                 
